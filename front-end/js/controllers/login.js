1240d1e9cbe3d72d02e95a7e443386b31eaf247a
Daniel Muñoz <daniel@danielmunoz.cr>
2016-03-30 23:42:23 -0600

    login validations

--- front-end/js/controllers/login.js
+++ front-end/js/controllers/login.js
@@ -10,10 +10,10 @@ angular.module('practicaPHP01.controllers')
                 /**
                  * TODO: Implementar
                  * Pasos
-                 * - Verifique si el usuario tiene una sesión activa.
+                 * - Verifique si el usuario tiene una sesión activa. -- DONE
                  * - Maneje los siguientes escenarios:
-                 *  - El usuario tiene una sesión activa, envie el usuario a la ruta de `home`.
-                 *  - No tiene una sesión activa, permita que la pagina de inicio de sesión funcione normalmente.
+                 *  - El usuario tiene una sesión activa, envie el usuario a la ruta de `home`. -- DONE
+                 *  - No tiene una sesión activa, permita que la pagina de inicio de sesión funcione normalmente. -- DONE
                  * - En el segundo caso, permítale al usuario iniciar una sesión en el sistema.
                  * - Agregue las validaciones necesarias: contenido vacio, correo en formato de correo.
                  * - Provea mensajes de error descriptivos.
@@ -22,21 +22,28 @@ angular.module('practicaPHP01.controllers')
                 // Checks if the user has an active session
                 var isLoggedIn = UserService.isLoggedIn();
 
+                // If there's a logged in user then redirect to home
                 if (isLoggedIn.success) {
                     $location.path("/home");
                 }
 
                 $scope.loginUser = function () {
-                    var user = {
-                        email : $scope.user.email,
-                        password : $scope.user.password
-                    };
+                    var user = {}
+
+                    // Checks if the the values exists
+                    if ($scope.email !== "") {
+                        user.email = $scope.email;
+                    }
+
+                    if ($scope.password !== "") {
+                        user.password = $scope.email;
+                    }
 
                     console.log(user);
 
-                    if ($scope.loginForm.$valid) {
-                        UserService.register(user);
-                    }
+                    // if ($scope.loginForm.$valid) {
+                    //     UserService.register(user);
+                    // }
                 };
             };
 
